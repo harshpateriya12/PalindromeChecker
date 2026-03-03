bbeefe23169d5e5c8c18a08d048d52a3bceda420
harshpateriya12 <harsh.pateriya1@gmail.com>
2026-03-03 15:56:08 -0800

    Coded UC1 to print welcome message

---
+++ PalindromeApp/src/com/PalindromeChecker/UCONEPalindromeChecker.java
@@ -0,0 +1,15 @@
+package com.PalindromeChecker;
+
+public class UCONEPalindromeChecker {
+	public static void initialize() {
+		System.out.println("Welcome to the palindrome checker app");
+		System.out.println("Version:1.0");
+		System.out.println("System Initialized successfully");
+	}
+	
+	public static void main(String[] args) {
+		UCONEPalindromeChecker.initialize();
+	}
+
+
+}
